ae75b00edc2485b56e9481990872cecfd26ddef5
Florian Großmann <florian.grossmann@widas.de>
2020-06-25 11:29:01 +0200

    https://gitlab.widas.de/cidaas-public-devkits/cidaas-public-devkit-documentation/-/issues/70 refactor sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,87 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
 import 'package:cidaassdkflutter/cidaassdkflutter.dart';
-import "./userInfoScreen.dart";
-import 'package:provider/provider.dart';
-import "./LoggedInScreen.dart";
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'LoggedInScreen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    App(),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatefulWidget {
+  static final String route = "/startPage";
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    printInitAuth();
+  }
+
+  printInitAuth() async {
+    print("isauth on init: " + (await CidaasLoginProvider.isAuth()).toString());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.orange,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return BlocProvider<AuthenticationBloc>(
+      create: (context) {
+        return AuthenticationBloc();
+      },
+      child: MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('App'),
+            ),
+            body: MyCidaasImpl()),
+        routes: {
+          App.route: (context) => App(),
+          LoggedInScreen.route: (context) => LoggedInScreen()
+        },
       ),
-      home: MyHomePage(title: 'Cidaas Flutter SDK Example'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyCidaasImpl extends Cidaas {
+  @override
+  Widget getLoggedInScreen({tokenEntity}) {
+    return LoggedInScreen(tokenEntity: tokenEntity);
+  }
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Duration duration;
-  bool _isLoggedIn = false;
-
-  /*CidaasLoginProvider _loginProvider = new CidaasLoginProvider();
-
-  void login() async {
-    _isLoggedIn = await _loginProvider.refreshLoginFromCache();
-  }*/
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_) => new CidaasLoginProvider())
-        ],
-        child: Consumer<CidaasLoginProvider>(
-            builder: (ctx, _cidaasLoginProvider, _) => MaterialApp(
-                  title: widget.title,
-                  home: _cidaasLoginProvider.isAuth
-                      ? Navigator.of(context)
-                      .pushReplacementNamed(LoggedInScreen.route)
-                      : LoginHandleScreen(LoggedInScreen.route),
-            routes: {
-              LoggedInScreen.route: (ctx) => LoggedInScreen() //Register your reroute Page here
-        }),
-        ),
+  Widget getSplashScreen() {
+    return Center(
+      //child: CircularProgressIndicator(),
+      child: Text('Please wait'),
     );
+  }
+
+  @override
+  Widget getLoggedOutScreen({context}) {
+    return Center(
+        child: RaisedButton(
+      child: Text('Login'),
+      onPressed: () {
+        CidaasLoginProvider.doLogin(context);
+      },
+    ));
+  }
+
+  @override
+  Widget getAuthenticationFailureScreen({errorMessage}) {
+    return Scaffold(
+        body: Center(
+      child: Text('$errorMessage', style:
+        TextStyle(
+        fontWeight: FontWeight.bold,
+        color: Colors.red)),
+    ));
   }
 }
--- lib/user_info_entity.dart
+++ lib/user_info_entity.dart
@@ -2,26 +2,42 @@ import 'package:flutter/material.dart';
 
 class UserInfoEntity with ChangeNotifier {
   String email;
+  String mobileNumber;
+  String picture;
+  String givenName;
+  String familyName;
   String sub;
+  String gender;
+  String birthYear;
+
   UserInfoEntity(
       {this.email,
-      this.sub
-      });
+      this.mobileNumber,
+      this.picture,
+      this.givenName,
+      this.familyName,
+      this.sub,
+      this.birthYear,
+      this.gender});
+
   UserInfoEntity.fromJson(Map<String, dynamic> json)
       : sub = json['sub'],
-        email = json['email'];
+        givenName = json['given_name'],
+        email = json['email'],
+        familyName = json['family_name'],
+        picture = json['picture'],
+        mobileNumber = json['mobile_number'],
+        birthYear = json['birthYear'],
+        gender = json['gender'];
 
   Map<String, dynamic> toJson() => {
         'sub': sub,
-        'email': email
+        'given_name': givenName,
+        'family_name': familyName,
+        'email': email,
+        'picture': picture,
+        'mobile_number': mobileNumber,
+        'birthYear': birthYear,
+        'gender': gender
       };
-
-  Future<void> updateUserEntity(UserInfoEntity updatedEntity) async {
-    if (updatedEntity != null) {
-      email = updatedEntity.email;
-      sub = updatedEntity.sub;
-      notifyListeners();
-    }
-  }
-
 }
